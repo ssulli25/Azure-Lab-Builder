95e7434d63a0e7c75b5e73a7bd3a2514f8211b9c
ssulli25 <seasulli25@gmail.com>
2025-03-09 13:51:02 -0500

    Added Route table for app gateway in spoke.

--- terraform/spoke/main.tf
+++ terraform/spoke/main.tf
@@ -523,6 +523,24 @@ resource "azurerm_route_table" "fw_route_table" {
   }
 }
 
+resource "azurerm_route_table" "appgw_route_table" {
+  count               = var.FwEnabled ? 1 : 0
+  name                = "spoke-route-table-appgw"
+  resource_group_name = azurerm_resource_group.network_rg.name
+  location            = azurerm_resource_group.network_rg.location
+
+  route {
+    name                   = "default-route"
+    address_prefix         = "0.0.0.0/0"
+    next_hop_type          = "Internet"
+  }
+  route {
+    name           = "local-route"
+    address_prefix = tolist(azurerm_virtual_network.vnet.address_space)[0]
+    next_hop_type  = "VnetLocal"
+  }
+}
+
 resource "azurerm_subnet_route_table_association" "web_route_table_association" {
   subnet_id      = azurerm_subnet.web_subnet.id
   route_table_id = azurerm_route_table.fw_route_table[0].id
@@ -530,7 +548,7 @@ resource "azurerm_subnet_route_table_association" "web_route_table_association" 
 
 resource "azurerm_subnet_route_table_association" "appgw_route_table_association" {
   subnet_id      = azurerm_subnet.appgw_subnet.id
-  route_table_id = azurerm_route_table.fw_route_table[0].id
+  route_table_id = azurerm_route_table.appgw_route_table[0].id
 }
 
 resource "azurerm_subnet_route_table_association" "app_route_table_association" {
