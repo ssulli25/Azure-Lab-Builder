5632d936c5b692cbbb527179fede2c2670dc1e8c
ssulli25 <seasulli25@gmail.com>
2025-07-11 10:25:42 -0500

    Updating script to parameter array and testing.

--- scripts/check-job-status.sh
+++ scripts/check-job-status.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 # filepath: scripts/check-job-status.sh
 
-status="$1"
-
-if [[ "$status" == "failure" ]]; then
-  echo "Previous job failed. Skipping this job."
-  exit 78
-elif [[ "$status" == "skipped" ]]; then
-  echo "Previous job was skipped due to a spoke environment being destroyed. Continuing."
-else
-  echo "Previous job succeeded. Continuing."
-fi
+for status in "$@"; do
+  if [[ "$status" == "failure" ]]; then
+    echo "A previous job failed. Skipping this job."
+    exit 78
+  elif [[ "$status" == "cancelled" ]]; then
+    echo "A previous job was cancelled. Skipping this job."
+    exit 78
+  elif [[ "$status" == "skipped" ]]; then
+    echo "A previous job was skipped. Continuing."
+  else
+    echo "A previous job succeeded. Continuing."
+  fi
+done
