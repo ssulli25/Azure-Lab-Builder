dcb44ea08f8aab445ac342d3e2d69d3776b3fd15
ssulli25 <seasulli25@gmail.com>
2025-07-10 22:41:26 -0500

    Updated skip messaging and removed exit code.

--- scripts/check-job-status.sh
+++ scripts/check-job-status.sh
@@ -7,8 +7,7 @@ if [[ "$status" == "failure" ]]; then
   echo "Previous job failed. Skipping this job."
   exit 78
 elif [[ "$status" == "skipped" ]]; then
-  echo "Previous job was skipped. Skipping this job."
-  exit 78
+  echo "Previous job was skipped due to a spoke environment being destroyed. Continuing."
 else
   echo "Previous job succeeded. Continuing."
 fi
