e5abf186dae654582af54e867cd04fbd626f1dbf
ssulli25 <seasulli25@gmail.com>
2025-07-24 11:17:30 -0500

    Naming and description updates.

--- terraform/spoke/main.tf
+++ terraform/spoke/main.tf
@@ -144,7 +144,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "appgw_subnet" {
-  name                 = "appgw-subnet"
+  name                 = "web-appgw-subnet"
   resource_group_name  = azurerm_resource_group.network_rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.AppGwSubnetPrefix
