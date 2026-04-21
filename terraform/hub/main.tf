aaf90f4443898feea245ff431aeab44df1e7039a
ssulli25 <26532577+ssulli25@users.noreply.github.com>
2026-04-21 15:20:28 -0500

    Fix: add lifecycle ignore for ip_tags on firewall public IP to prevent forced replacement.

--- terraform/hub/main.tf
+++ terraform/hub/main.tf
@@ -276,6 +276,10 @@ resource "azurerm_public_ip" "firewall_pip" {
   location            = azurerm_resource_group.network_rg.location
   allocation_method   = "Static"
   sku                 = "Standard"
+
+  lifecycle {
+    ignore_changes = [ip_tags]
+  }
 }
 
 resource "azurerm_firewall_policy" "firewall_policy" {
